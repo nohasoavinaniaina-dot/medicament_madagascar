f5b8e0747a3f00caa2c239f876028d02cfdd767f
nohasoavinaniaina-dot <nohasoavina.niaina@gmail.com>
2026-06-11 21:39:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'medicaments-mada-v73';
+const CACHE_NAME = 'medicaments-mada-v74';
 const BASE = '/medicament_madagascar';
 const ASSETS = [
   BASE + '/',
@@ -8,15 +8,12 @@ const ASSETS = [
   BASE + '/icon-512.png',
   BASE + '/apple-touch-icon.png'
 ];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   const url = new URL(e.request.url);
